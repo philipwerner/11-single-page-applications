5a96ad26dc6bb5248d66aaf6c6359588911a409d
Philip Werner <philip.r.werner@gmail.com>
2017-08-28 14:18:55 -0700

    fixed issue with script tags for page.js and routes.js

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -3,13 +3,14 @@ var app = app || {};
 
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-    articleController.init = function() {
-      app.articleView.initIndexPage();
-      $('/articles').show();
-      $('/about').hide();
-    };
+  articleController.init = function() {
+    console.log('article page');
+    app.Article.fetchAll(app.articleView.initIndexPage);
+    $('#articles').show();
+    $('#about').hide();
+  };
   module.articleController = articleController;
 })(app);
